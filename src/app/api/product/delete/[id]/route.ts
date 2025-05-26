4e2183fabfb19f559fc89012d7d4a94bdcf5a6f5
Risqi Fahreal <106820529+Xfhreall@users.noreply.github.com>
2025-05-26 13:16:13 +0700

    fix: delete route params

--- src/app/api/product/delete/[id]/route.ts
+++ src/app/api/product/delete/[id]/route.ts
@@ -1,22 +1,25 @@
 import prisma from '@/shared/lib/prisma';
 import { type NextRequest, NextResponse } from 'next/server';
 
-export async function DELETE(
-  req: NextRequest,
-  context: { params: { id: string } },
-) {
-  const { id } = context.params;
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function DELETE(req: NextRequest, { params }: Context) {
+  const { id } = await params;
 
   try {
     const deletedProduct = await prisma.product.delete({
-      where: { id: id },
+      where: { id },
     });
 
     return NextResponse.json(
       { message: 'Product deleted successfully', product: deletedProduct },
       { status: 200 },
     );
-  } catch {
+  } catch (error) {
     return NextResponse.json(
       { error: 'Product not found or could not be deleted' },
       { status: 404 },
