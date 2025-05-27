5200da316d825e7af3a95a11d8047809eadc1d92
Risqi Fahreal <106820529+Xfhreall@users.noreply.github.com>
2025-05-27 17:25:31 +0700

    fix: new user failed fetch product on dashboard

--- src/app/api/product/user/route.ts
+++ src/app/api/product/user/route.ts
@@ -8,40 +8,43 @@ export async function GET(req: NextRequest) {
   if (!userId) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
-  const clerk = await clerkClient();
-  const clerkUser = await clerk.users.getUser(userId);
-  const email = clerkUser.emailAddresses[0]?.emailAddress;
-
-  if (!email) {
-    return NextResponse.json(
-      { error: 'Email not found from Clerk' },
-      { status: 400 },
-    );
-  }
-
-  const user = await prisma.user.findUnique({
-    where: { email },
-  });
 
   try {
-    if (!user) {
-      return NextResponse.json({ error: 'Product empty' }, { status: 404 });
+    const clerk = await clerkClient();
+    const clerkUser = await clerk.users.getUser(userId);
+    const email = clerkUser.emailAddresses[0]?.emailAddress;
+
+    if (!email) {
+      return NextResponse.json(
+        { error: 'Email not found from Clerk' },
+        { status: 400 },
+      );
     }
 
-    if (user?.role === 'ADMIN') {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return NextResponse.json([]);
+    }
+
+    if (user.role === 'ADMIN') {
       const products = await prisma.product.findMany({
         orderBy: { createdAt: 'asc' },
       });
       return NextResponse.json(products);
     }
 
+    // Return user's products
     const products = await prisma.product.findMany({
       where: {
-        userId: user?.id,
+        userId: user.id,
       },
     });
     return NextResponse.json(products);
   } catch (error) {
+    console.error('Failed to fetch products:', error);
     return NextResponse.json(
       { error: 'Failed to fetch products' },
       { status: 500 },
